7e69026bb73cb197ff167ee4f3294458421ef114
KpryKorn <roffinisacha@gmail.com>
2025-07-25 13:42:13 +0200

    feat: implémenter la page de détails des tâches avec affichage des informations et actions disponibles

--- frontend/src/app/components/pages/task-detail-page/task-detail-page.component.ts
+++ frontend/src/app/components/pages/task-detail-page/task-detail-page.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, computed, inject, OnInit, signal } from '@angular/core';
+import { TaskService } from '../../../services/tasks/task.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-task-detail-page',
   imports: [],
-  templateUrl: './task-detail-page.component.html'
+  templateUrl: './task-detail-page.component.html',
 })
-export class TaskDetailPageComponent {
+export class TaskDetailPageComponent implements OnInit {
+  private readonly route = inject(ActivatedRoute);
+  readonly taskService = inject(TaskService);
+  readonly taskId = signal<string>('');
 
+  readonly task = computed(() => {
+    const id = this.taskId();
+    const tasks = this.taskService.tasks();
+    return tasks.find((task) => task.id.toString() === id) || null;
+  });
+
+  ngOnInit() {
+    const id = this.route.snapshot.paramMap.get('id');
+    if (id) {
+      this.taskId.set(id);
+    }
+  }
+
+  toggleTask(id: string) {
+    this.taskService.toggleTaskDone(id).subscribe();
+  }
 }
